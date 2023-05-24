407bd6408782f519ae22b0d4c113680fd6328727
Kristjan Lääne <xonproject@gmail.com>
2023-05-24 18:12:13 +0300

    rendering images

--- src/AnimalShow.jsx
+++ src/AnimalShow.jsx
@@ -1,3 +1,24 @@
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+  cat,
+  bird,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
 export const AnimalShow = ({ type }) => {
-  return <div>{type}</div>;
+  return (
+    <div>
+      <img src={svgMap[type]} alt="animal" />
+    </div>
+  );
 };
